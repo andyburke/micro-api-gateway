dd7e3e10b701d2b25e53cc58c9e1a65af2568782
Andy Burke <andy.burke@mailbox.earth>
2019-02-21 23:21:48 -0800

    skip some headers in sig

--- plugins/sign-requests.js
+++ plugins/sign-requests.js
@@ -11,10 +11,17 @@ module.exports = function( _options ) {
 
         input.proxied_request.setHeader( 'x-micro-api-gateway-signature-time', `${ +new Date() }` );
 
-        const request_as_string = [ input.proxied_request.method, input.proxied_request.path, json_stable_stringify( input.proxied_request.getHeaders() ) ].join( ':::' );
-        console.log( request_as_string );
+        const headers_to_sign = extend( true, {}, input.proxied_request.getHeaders() );
+        delete headers_to_sign[ 'connection' ];
+        delete headers_to_sign[ 'transfer-encoding' ];
+
+        const request_as_string = [ input.proxied_request.method, input.proxied_request.path, json_stable_stringify( headers_to_sign ) ].join( ':::' );
         const request_hash = crypto.createHash( 'SHA256' ).update( request_as_string ).digest( 'base64' );
         const request_hash_signature = crypto.createSign( 'RSA-SHA256' ).update( request_hash ).sign( options.key, 'base64' );
+
+        console.log( `REQ: ${ request_as_string }` );
+        console.log( `HASH: ${ request_hash }` );
+        console.log( `HASH_SIG: ${ request_hash_signature }` );
 
         input.proxied_request.setHeader( 'x-micro-api-gateway-request-hash',  request_hash );
         input.proxied_request.setHeader( 'x-micro-api-gateway-signature', request_hash_signature );
